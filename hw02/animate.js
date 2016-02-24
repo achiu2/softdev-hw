90e0b8e496650fcca23843402f67c8f934d73cfe
amandac88 <amanda.chiu88@gmail.com>
2016-02-24 10:02:25 -0500

    add hw 02

---
+++ hw02/animate.js
@@ -0,0 +1,13 @@
+//var c is ref to the element id "ftb2maga" in index.html
+var c = document.getElementById("ftb2maga");
+//create an object ctx so can write methods for drawing on the canvas
+var ctx = c.getContext("2d");
+
+/**
+if ( growing ) { radius  = radius +1 }
+else { radius = radius - 1 }
+
+if (radius == (c.width / 2) )
+growing - false;
+else if (radius == 0) {}
+**/
