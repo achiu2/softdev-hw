488b73588176af720c6a6d4113cca3b0bf886593
amandac88 <amanda.chiu88@gmail.com>
2016-04-04 09:47:40 -0400

    add hw07 closures

---
+++ hw07/closures.py
@@ -0,0 +1,13 @@
+def repeat(str):
+    return lambda n: str * n
+
+print repeat('cool')(3)
+
+
+def r1(n):
+    return repeat('hello')(n)
+def r2(n):
+    return repeat('goodbye')(n)
+
+print r1(2)
+print r2(2)
